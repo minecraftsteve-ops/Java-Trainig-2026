92f69853a983bcf7c4c19041cef5ccc10e32adc2
Steven <zhuqicheng873@gmail.com>
2026-08-23 14:21:20 -0700

    c

---
+++ Optional.java
@@ -0,0 +1,15 @@
+public class Optional {
+    public static void main(String[] args) {
+
+        //trapezoid
+        double base1 = 16;
+        double base2 = 7;
+        double height = 8;
+
+        double bases = base1 + base2;
+        
+        System.out.println(.5 * bases * height);
+        
+
+    }
+}
---
+++ Trapezoid.java
@@ -0,0 +1,11 @@
+public class Trapezoid {
+    public static void main(String[] args) {
+        double A = 3;
+        double B = 4;
+        double Height = 3;
+        
+        double AB = A+B;
+
+        System.out.println("The area of the trapezod is " + 0.5*AB*Height);
+    }
+}
